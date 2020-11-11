e4b3ab0ee2b9ecc9da0cdeee029f666c1135e399
Mason Howe <mason.a.howe@gmail.com>
2020-11-10 18:03:57 -0700

    concatinate arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,37 @@
 // Assignment code here
 
-//global var
-var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var specialChars = [" ", "!", "#", "$", "%", "&", "\'", "\\", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
+//global items
+const numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+const alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+const specialChars = [" ", "!", "#", "$", "%", "&", "\'", "\\", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
 
 
 //logic functions
 function generatePassword(passwordLength) {
   let passwordArray = []
+
+  // local copy of the items to not affect the global variables. Likely change this later. 
+  let num = numeric
+  let alphaU = alphaUpper
+  let special = specialChars
   // let passwordLength = passwordLength
 
   //checkbox status
-  let alphabeticBool = document.getElementById("numeric").checked
+  let alphabeticBool = document.getElementById("letters").checked
   let symoblicBool = document.getElementById("symbols").checked
 
+  if (alphabeticBool == true) {
+
+    num = num.concat(alphaU)
+  }
+  if (symoblicBool == true) {
+
+    num = num.concat(special)
+  }  
 
   for (i = 0; i < passwordLength; i++) {
-    let number = Math.floor(Math.random() * 10)
-    passwordArray.push(number)
+    let randomItem = num[Math.floor(Math.random() * num.length)];
+    passwordArray.push(randomItem)
 
   }
 
